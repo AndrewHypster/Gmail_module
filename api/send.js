a399be6316a96adea306408bb2cc604ebb1ab0f0
Андрій Гречух <119750751+AndrewHypster@users.noreply.github.com>
2025-07-29 20:54:44 +0300

    Create send.js

---
+++ api/send.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const nodemailer = require("nodemailer");
+const cors = require("cors");
+
+const app = express();
+const PORT = 7700;
+
+app.use(cors());
+app.use(express.json());
+
+app.post("/api/send", async (req, res) => {
+  if (req.method !== "POST") {
+    return res.status(405).send("Method Not Allowed");
+  }
+  
+  const { service, auth, from, to, subject, text } = req.body;
+  
+  const transporter = nodemailer.createTransport({
+    service,
+    auth,
+  });
+  
+  const mailOptions = { from, to, subject, text };
+  
+  try {
+    await transporter.sendMail(mailOptions);
+    res.status(200).send("Email надіслано!");
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Помилка при надсиланні");
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Сервер працює на http://localhost:${PORT}`);
+});
