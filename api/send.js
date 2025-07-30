d3b5eebd7eecb7685470d40187963faac30d990c
Андрій Гречух <119750751+AndrewHypster@users.noreply.github.com>
2025-07-30 11:38:06 +0300

    Update send.js

--- api/send.js
+++ api/send.js
@@ -11,12 +11,12 @@ app.use(express.json());
 // C O N N E C T I O N
 app.post("/api/auth", async (req, res) => {
   if (req.method !== "POST") {
-    return res.status(500).send({
+    return res.status(200).send({
       message: "Потрібен POST запит"
     });
   }
   if (req.headers.api_key !== process.env.API_KEY) {
-    return res.status(500).send({
+    return res.status(200).send({
       message: "Невірний api key :("
     });
   }
