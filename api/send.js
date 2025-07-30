6db09ac095a0416a90512920d15a71cdcc9d746b
Андрій Гречух <119750751+AndrewHypster@users.noreply.github.com>
2025-07-30 10:38:17 +0300

    Update send.js

--- api/send.js
+++ api/send.js
@@ -25,7 +25,7 @@ app.post("/api/send", async (req, res) => {
   try {
     await transporter.sendMail(mailOptions);
     res.status(200).send({
-      message: req
+      message: req.headers
     });
   } catch (err) {
     console.error(err);
